2ef344e65fcc0b2d556ca951d3a8b953755eca46
Aman Rai <amanraihot@gmail.com>
2021-03-25 19:23:10 +0530

    Added Dto and configured authentication

---
+++ demo/src/main/java/com/example/demo/service/UserDetailServiceImp.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repo.UserRepo;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+
+@Service
+public class UserDetailsServiceImp implements UserDetailsService {
+
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public UserDetails loadUserByUsername(String username)
+    {
+        Optional<User> userOptional = userRepo.findByUsername(username);
+
+        User user = userOptional.orElseThrow(()->new UsernameNotFoundException("No user found"));
+
+        return new org.springframework.security
+                .core.userdetails.User(user.getUserName(), user.getPassword(),
+                user.isActive(), true, true,
+                true, getAuthorities("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return singletonList(new SimpleGrantedAuthority(role));
+    }
+
+}
